a6d9b04389b8cf42a5f8a40e59a15b515f997166
Wariy <97937739+Wariy@users.noreply.github.com>
2022-01-25 13:38:58 +0700

    Update page1.php

--- page1.php
+++ page1.php
@@ -1,7 +1,8 @@
 <html>
       <head>
+            <title>งานที่ 1</title>
       </head>
       <body bgcolor="lightblue">
-                page1
+                page1ธรรมดา - - - <b>page1 หนา</b>
         </body>
 </html>
