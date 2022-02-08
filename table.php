6581781be8e95dd7aa6c91192d0777b79ceb4cb0
Wariy <97937739+Wariy@users.noreply.github.com>
2022-02-08 15:19:01 +0700

    Update table.php

--- table.php
+++ table.php
@@ -19,11 +19,18 @@
         <td><a href="https://twitter.com/ryuvachirawich?t=1ZjOiMsK5VjXv4uhd3IRPA&s=07">twitter</a></td>
       </tr>  
       <tr>
-        <td>1</td>
-        <td>วชิรวิชญ์ อรัญธนวงศ์</td>
-        <td>ริว</td>
-        <td><img src="https://s359.kapook.com/pagebuilder/3abe85c9-2299-4ddf-a98a-1b1416534684.jpg" width="130px"></td>
-        <td><a href="https://twitter.com/ryuvachirawich?t=1ZjOiMsK5VjXv4uhd3IRPA&s=07">twitter</a></td>
+        <td>3</td>
+        <td>ลภัส งามเชวง</td>
+        <td>เติร์ด</td>
+        <td><img src="http://entertain.teenee.com/thaistar/img6/1489292.jpg" width="130px"></td>
+        <td><a href="https://twitter.com/third_n?t=vSoUVj4rVe8hKd6HHiukrw&s=07">twitter</a></td>
+      </tr>  
+      <tr>
+        <td>3</td>
+        <td>ลภัส งามเชวง</td>
+        <td>เติร์ด</td>
+        <td><img src="http://entertain.teenee.com/thaistar/img6/1489292.jpg" width="130px"></td>
+        <td><a href="https://twitter.com/third_n?t=vSoUVj4rVe8hKd6HHiukrw&s=07">twitter</a></td>
       </tr>  
     </table>
   </body> 
