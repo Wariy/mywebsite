01b85638a7a97f3ef3b9fe2cc431a65b66d39b0a
Wariy <97937739+Wariy@users.noreply.github.com>
2022-02-08 15:09:33 +0700

    Update table.php

--- table.php
+++ table.php
@@ -8,11 +8,15 @@
         <th>ลำดับ</th>
         <th>ชื่อ - นามสกุล</th>
         <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
       </tr>
       <tr>
         <td>1</td>
-        <td>ยิ่งคุณ รอดทิม</td>
-        <td>ขม</td>
+        <td>วชิรวิชญ์ อรัญธนวงศ์</td>
+        <td>ริว</td>
+        <td><img src="https://s359.kapook.com/pagebuilder/3abe85c9-2299-4ddf-a98a-1b1416534684.jpg" width="130px"></td>
+        <td><a href="https://twitter.com/ryuvachirawich?t=1ZjOiMsK5VjXv4uhd3IRPA&s=07">twitter</a></td>
       </tr>  
     </table>
   </body> 
