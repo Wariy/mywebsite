b2c39cb4ae5273103cd1a2876fa5dcea05e3ff36
Wariy <97937739+Wariy@users.noreply.github.com>
2022-02-15 23:14:22 +0700

    Update page2.php

--- page2.php
+++ page2.php
@@ -1,9 +1,25 @@
 <html>
 	<head>
 		<title>การตกแต่งเว็บไซต์ด้วย CSS</title>
+		<style>
+			#box1{
+				width: 800px;
+				height: 350px;
+			}
+			.box{
+				background-color: lightblue;
+			}
+                        body{
+                                background-color: pink;
+			}
+                        h1{
+                                color: yellow;
+			}
+		</style>
 	</head>
 	<body>
-		<p style="color:green;">ข้อความนี้สีเขียวปกติ</p>
-		<p style="color:#82933f;">ข้อความนี้สีเขียวรหัสสี</p>
+		<div id="box1" class="box">
+			<h1>นางสาววริยา พรมท่อน เลขที่ 22 ม.4/13</h1>
+		</div>
 	</body>
 </html>
