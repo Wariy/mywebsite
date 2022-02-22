826baae29da46680eed7e7ea199e2077432924cc
Wariy <97937739+Wariy@users.noreply.github.com>
2022-02-22 14:20:29 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>กำหนดรูปแบบตัวอักษร</title>
+  <style>
+    @font-face{
+      font-family: myFont1;
+      src: url(SOV_sorm2496.woff)
+    }
+    @font-face{
+      font-family: myFont2;
+      src: url(SOV_sorm2496.woff)
+    }
+    #font1{
+	font-family: myFont1;    
+    }
+  </style>
+</head>
+<body>
+  <h1 id="font1">นางสาววริยา พรมท่อน ม.4/13 เลขที่ 22 เลขประจำตัว 57041</h1>
+  <h1 id="font2">นางสาววริยา พรมท่อน ม.4/13 เลขที่ 22 เลขประจำตัว 57041</h1>
+</body>
+</html>
